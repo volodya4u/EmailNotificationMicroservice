cfeab47e47cf995cb8bb7c2c6c65b1391bc91c30
volodymyrhrabovenskyi <volodya4u@gmail.com>
2025-08-16 00:21:19 +0300

    Add entity class

---
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/io/ProcessedEventEntity.java
@@ -0,0 +1,32 @@
+package com.appsdeveloperblog.ws.emailnotification.io;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="processed-events")
+public class ProcessedEventEntity implements Serializable {
+
+    private static final long serialVersionUID = 3633493322337482057L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String messageId;
+
+    @Column(nullable = false)
+    private String productId;
+
+
+}
