2a10809bec1778d342c8efb1469934818beb1861
volodymyrhrabovenskyi <volodya4u@gmail.com>
2025-08-16 01:05:08 +0300

    Store ProductCreatedEvent to database

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -3,7 +3,10 @@ package com.appsdeveloperblog.ws.emailnotification.handler;
 import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.emailnotification.error.NotRetryableException;
 import com.appsdeveloperblog.ws.emailnotification.error.RetryableException;
+import com.appsdeveloperblog.ws.emailnotification.io.ProcessedEventEntity;
+import com.appsdeveloperblog.ws.emailnotification.io.ProcessedEventRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
@@ -12,6 +15,7 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
@@ -23,10 +27,15 @@ public class ProductCreatedEventHandler {
 
     private final RestTemplate restTemplate;
 
-    public ProductCreatedEventHandler(RestTemplate restTemplate) {
+    private ProcessedEventRepository processedEventRepository;
+
+    public ProductCreatedEventHandler(RestTemplate restTemplate,
+                                      ProcessedEventRepository processedEventRepository) {
         this.restTemplate = restTemplate;
+        this.processedEventRepository = processedEventRepository;
     }
 
+    @Transactional
     @KafkaHandler
     public void handle(@Payload ProductCreatedEvent productCreatedEvent,
                        @Header("messageId") String messageId,
@@ -49,6 +58,13 @@ public class ProductCreatedEventHandler {
         } catch (HttpServerErrorException e) {
             log.error(e.getMessage());
             throw new NotRetryableException(e);
+        }
+
+        // Save a unique message id in a database table
+        try {
+            processedEventRepository.save(new ProcessedEventEntity(messageId, productCreatedEvent.getProductId()));
+        } catch (DataIntegrityViolationException ex) {
+            throw new NotRetryableException(ex);
         }
     }
 }
