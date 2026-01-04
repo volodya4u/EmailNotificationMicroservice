3f0378c45b3852120a73cdb3c0c4503478f9b5a8
volodya4u <volodya4u@hotmail.com>
2026-01-04 03:04:06 +0200

    Add arrange section of the test

--- src/test/java/com/appsdeveloperblog/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/ws/emailnotification/ProductCreatedEventHandlerIntegrationTest.java
@@ -1,18 +1,32 @@
 package com.appsdeveloperblog.ws.emailnotification;
 
 import com.appsdeveloperblog.ws.core.events.ProductCreatedEvent;
+import com.appsdeveloperblog.ws.emailnotification.io.ProcessedEventEntity;
+import com.appsdeveloperblog.ws.emailnotification.io.ProcessedEventRepository;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
 @EmbeddedKafka
 @SpringBootTest(properties="spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}")
 public class ProductCreatedEventHandlerIntegrationTest {
+
+    @MockitoBean
+    ProcessedEventRepository processedEventRepository;
+
+    @MockitoBean
+    RestTemplate restTemplate;
 
     @Test
     public void testProductCreatedEventHandler_OnProductCreated_HandlesEvent() {
@@ -36,6 +50,24 @@ public class ProductCreatedEventHandlerIntegrationTest {
 
         record.headers().add("messageId", messageId.getBytes());
         record.headers().add(KafkaHeaders.RECEIVED_KEY, messageKey.getBytes());
+
+        ProcessedEventEntity processedEventEntity = new ProcessedEventEntity();
+        when(processedEventRepository.findByMessageId(anyString()))
+                .thenReturn(processedEventEntity);
+        when(processedEventRepository.save(any(ProcessedEventEntity.class)))
+                .thenReturn(null);
+
+        String responseBody = "{\"key\":\"value\"}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<String> responseEntity
+                = new ResponseEntity<>(responseBody, headers, HttpStatus.OK);
+
+        when(restTemplate.exchange(
+                any(String.class),
+                any(HttpMethod.class),
+                isNull(), eq(String.class)))
+        .thenReturn(responseEntity);
 
         // Act
 
