e906f5d6f74e7e1cfeefbf5856811e4ddfdff781
volodya4u <volodya4u@hotmail.com>
2026-01-04 03:56:33 +0200

    Add kafka consumer.auto-offset-reset property

--- src/main/java/com/appsdeveloperblog/ws/emailnotification/KafkaConsumerConfiguration.java
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/KafkaConsumerConfiguration.java
@@ -39,6 +39,7 @@ public class KafkaConsumerConfiguration {
         config.put(JsonDeserializer.TRUSTED_PACKAGES,
                 environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
         config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("spring.kafka.consumer.auto-offset-reset"));
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
